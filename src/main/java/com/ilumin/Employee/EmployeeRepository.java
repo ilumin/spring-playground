e37b093c60abf4095d60025cf2c1b53d4fd77099
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-10-03 16:02:21 +0700

    REMOVE unused

--- src/main/java/com/ilumin/Employee/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ilumin.Employee;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
-public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
-}
